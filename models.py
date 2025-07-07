b416a7bfe7c108b8ef6593c9944e1445476af198
xyrobin <xianyirobin@163.com>
2025-07-07 17:21:04 +0800

    20250707 修改了工单状态字段

--- models.py
+++ models.py
@@ -9,43 +9,57 @@ class ProductionOrderService:
         self.cursor = self.connection.cursor()
 
     def get_pending_orders(self):
-        # """获取所有待排产工单（状态=0）并按最早生产日期升序排序"""
+        # 排产-获取所有待排产工单（状态=待排产）并按最早生产日期升序排序
         query = """
           SELECT a.SCGDH,
-         a.GSXS,
-         a.JHKSRQ,
-         a.JHKSSJ,
-         a.BC,
-         a.ZZSCRQ,
-         a.ZZSCSJ,
-         a.JHRQ,
-         a.JHSJ,
-         b.KHMC,
-         b.PH     PH,
-         b.gg     GG,
-         b.JHZS   SL
-    FROM A_PC_SCGDWH_TAB a
-    left join A_PC_DPCSCGD_VW b
-      on a.scgdh = b.SCGDH
-   WHERE a.GDJHZT = 0
+                a.GSXS,
+                a.JHKSRQ,
+                a.JHKSSJ,
+                a.BC,
+                a.ZZSCRQ,
+                a.ZZSCSJ,
+                a.JHRQ,
+                a.JHSJ,
+                b.KHMC,
+                b.PH     PH,
+                b.gg     GG,
+                b.JHZS   SL
+            FROM A_PC_SCGDWH_TAB a
+            left join A_PC_DPCSCGD_VW b
+            on a.scgdh = b.SCGDH
+        WHERE a.GDJHZT = '待排产' and b.GXDL = 'BL'
         """
         self.cursor.execute(query)
         columns = [col[0].lower() for col in self.cursor.description]
         return [dict(zip(columns, row)) for row in self.cursor.fetchall()]
 
     def get_scheduled_orders(self):
-        # 获取所有已排产工单（状态=1）
+        # 排产-获取所有已排产工单（状态=已排产）
         query = """
-        SELECT SCGDH, GSXS, JHKSRQ, JHKSSJ 
-        FROM A_PC_SCGDWH_TAB 
-        WHERE GDJHZT = 1
+        SELECT a.SCGDH,
+                a.GSXS,
+                a.JHKSRQ,
+                a.JHKSSJ,
+                a.BC,
+                a.ZZSCRQ,
+                a.ZZSCSJ,
+                a.JHRQ,
+                a.JHSJ,
+                b.KHMC,
+                b.PH     PH,
+                b.gg     GG,
+                b.JHZS   SL
+            FROM A_PC_SCGDWH_TAB a
+            left join A_PC_DPCSCGD_VW b
+            on a.scgdh = b.SCGDH
+        WHERE a.GDJHZT = '已排产' and b.GXDL = 'BL'
         """
         self.cursor.execute(query)
         columns = [col[0].lower() for col in self.cursor.description]
         return [dict(zip(columns, row)) for row in self.cursor.fetchall()]
 
     def schedule_order(self, order_no, plan_date, shift, start_time):
-        # """更新工单状态为已排产"""
+        # 排产-更新工单状态为已排产
         try:
             # 检查班次是否合法
             if shift not in ["A", "B"]:
@@ -54,7 +68,7 @@ class ProductionOrderService:
             # 更新工单状态和排产信息
             update_sql = """
             UPDATE A_PC_SCGDWH_TAB
-            SET GDJHZT = 1, JHKSRQ = :plan_date, BC = :shift, JHKSSJ = :start_time
+            SET GDJHZT = '已排产', JHKSRQ = :plan_date, BC = :shift, JHKSSJ = :start_time
             WHERE SCGDH = :order_no
             """
             self.cursor.execute(
@@ -73,11 +87,11 @@ class ProductionOrderService:
             return {"success": False, "message": str(e)}
 
     def unschedule_order(self, order_no):
-        # """取消排产，将工单状态改回待排产"""
+        # 排产-取消排产，将工单状态改回待排产
         try:
             update_sql = """
             UPDATE A_PC_SCGDWH_TAB
-            SET GDJHZT = 0, JHKSRQ = NULL, BC = NULL, JHKSSJ = NULL
+            SET GDJHZT = '待排产', JHKSRQ = NULL, BC = NULL, JHKSSJ = NULL
             WHERE SCGDH = :order_no
             """
             self.cursor.execute(update_sql, {"order_no": order_no})
@@ -88,10 +102,10 @@ class ProductionOrderService:
             return {"success": False, "message": str(e)}
 
     def get_all_orders(self):
-        # 获取所有工单数据
+        # 排产-获取所有工单数据
         query = """
         SELECT SCGDH, JHRQ, JHSJ, YWY, YLKW, GSXS, JHKSRQ, JHKSSJ, BC, ZZSCRQ, ZZSCSJ, GDJHZT
-        FROM A_PC_SCGDWH_TAB
+        FROM A_PC_SCGDWH_TAB where and GXDL = 'BL'
         ORDER BY SCGDH
         """
         self.cursor.execute(query)
@@ -99,7 +113,7 @@ class ProductionOrderService:
         return [dict(zip(columns, row)) for row in self.cursor.fetchall()]
 
     def update_order(self, order_data):
-        # 更新工单数据
+        # 工单维护-更新工单数据
         try:
             update_sql = """
             UPDATE A_PC_SCGDWH_TAB
@@ -109,11 +123,11 @@ class ProductionOrderService:
             self.cursor.execute(
                 update_sql,
                 {
-                    "jhrq": order_data.get('jhrq'),
-                    "jhsj": order_data.get('jhsj'),
-                    "zzscrq": order_data.get('zzscrq'),
-                    "zzscsj": order_data.get('zzscsj'),
-                    "scgdh": order_data.get('scgdh')
+                    "jhrq": order_data.get("jhrq"),
+                    "jhsj": order_data.get("jhsj"),
+                    "zzscrq": order_data.get("zzscrq"),
+                    "zzscsj": order_data.get("zzscsj"),
+                    "scgdh": order_data.get("scgdh"),
                 },
             )
             self.connection.commit()
